80096c2d50de55feba84bed63dc600f0ea1523d1
Oli <oliivett2001@gmail.com>
2022-11-02 20:04:55 +1000

    fixed login and signup

--- website/auth.py
+++ website/auth.py
@@ -1,52 +1,46 @@
 
-import email
-from xmlrpc.client import boolean
-from flask import (
-    Blueprint, flash, render_template, request, url_for, redirect
-)
-# from matplotlib.pyplot import get
-from sqlalchemy import true
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from . import db
 from .models import User
-from flask_login import login_user, login_required, logout_user, current_user
-from flask import Blueprint, render_template
-
 
 # create a blueprint
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
-def login_page():
+def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.home_page'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Username does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_up_page():
+def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
         username = request.form.get('username')
+        email = request.form.get('email')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        dob = request.form.get('Dateofbirth')
-        username = request.form.get('username')
 
         user = User.query.filter_by(username=username).first()
         if user:
@@ -60,12 +54,12 @@ def sign_up_page():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password1, method='sha256',dob=dob))
+            new_user = User(username=username,email=email, password=generate_password_hash(
+                password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home_page'))
+            return redirect('/')
 
     return render_template('sign_up.html', user=current_user)
