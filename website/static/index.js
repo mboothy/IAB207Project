824ff4703d3e7e9d87bcbedf111726ea8dfe65c6
PiriCavanagh <82499709+PiriCavanagh@users.noreply.github.com>
2022-11-04 11:35:01 +1000

    positioned and styled error messages

--- website/static/index.js
+++ website/static/index.js
@@ -2,3 +2,11 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
     subMenu.classList.toggle("open-menu");
 }
+let alert = document.getElementById("alertid");
+function closealert() {
+    alert.classList.toggle("closealert");
+}
+let logout_message = document.getElementById("logout_messageid");
+function show_log_out_message() {
+    logout_message.classList.toggle("show");
+}
